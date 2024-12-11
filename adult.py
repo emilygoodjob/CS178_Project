20d54b1e04dfd9b1fa6415aece4b0015ba111a60
Zhengxuan Li <zli32@uci.edu>
2024-12-10 18:41:00 -0800

    first step

--- adult.py
+++ adult.py
@@ -9,22 +9,22 @@ pd.options.display.float_format = '{:.2f}'.format
 column_names = ['age', 'workclass', 'fnlwgt', 'education', 'education-num',
                 'marital-status', 'occupation', 'relationship', 'race', 'sex',
                 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country', 'income']
-train_data = pd.read_csv('adult/adult.data', header=None, names=column_names, na_values=' ?')
-test_data = pd.read_csv('adult/adult.test', header=None, names=column_names, skiprows=1, na_values=' ?')
+train_data = pd.read_csv('/Users/zhengxuanli/Learning/24 Fall/CS 178/finalproject/CS178_Project/adult/adult.data', header=None, names=column_names, na_values=' ?')
+test_data = pd.read_csv('/Users/zhengxuanli/Learning/24 Fall/CS 178/finalproject/CS178_Project/adult/adult.test', header=None, names=column_names, skiprows=1, na_values=' ?')
 
 # Numerical Features
-# print(train_data.describe())
+print(train_data.describe())
 
 # Categorical Distribution
-# print(train_data['education'].value_counts())
-# print(train_data['income'].value_counts())
-# print(train_data['workclass'].value_counts())
-# print(train_data['marital-status'].value_counts())
-# print(train_data['occupation'].value_counts())
-# print(train_data['relationship'].value_counts())
-# print(train_data['race'].value_counts())
-# print(train_data['sex'].value_counts())
-# print(train_data['native-country'].value_counts())
+print(train_data['education'].value_counts())
+print(train_data['income'].value_counts())
+print(train_data['workclass'].value_counts())
+print(train_data['marital-status'].value_counts())
+print(train_data['occupation'].value_counts())
+print(train_data['relationship'].value_counts())
+print(train_data['race'].value_counts())
+print(train_data['sex'].value_counts())
+print(train_data['native-country'].value_counts())
 
 # Corelation
 # corr = train_data.corr()
@@ -34,10 +34,10 @@ test_data = pd.read_csv('adult/adult.test', header=None, names=column_names, ski
 # plt.show()
 
 
-# education_income = pd.crosstab(train_data['education'], train_data['income'], normalize='index')
-# education_income.plot(kind='bar', stacked=True, figsize=(10, 6))
-# plt.title('Income Distribution by Education Level')
-# plt.xlabel('Education Level')
-# plt.ylabel('Proportion')
-# plt.show()
+education_income = pd.crosstab(train_data['education'], train_data['income'], normalize='index')
+education_income.plot(kind='bar', stacked=True, figsize=(10, 6))
+plt.title('Income Distribution by Education Level')
+plt.xlabel('Education Level')
+plt.ylabel('Proportion')
+plt.show()
 
